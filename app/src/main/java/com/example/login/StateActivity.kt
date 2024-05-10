b5187ceca7260511c4b79c55bed7442c00e420fd
Lastbullet0520 <wjswndgk@gmail.com>
2024-05-10 13:37:00 +0900

    삭제 버튼 추가; 일정 횟수 초과 또는 미만이면 버튼 비활성화

--- app/src/main/java/com/example/login/StateActivity.kt
+++ app/src/main/java/com/example/login/StateActivity.kt
@@ -18,23 +18,45 @@ class StateActivity :ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             Column {
-                ButtonReact()
+                var count by remember {
+                    mutableStateOf(0)
+                }
+                var SaveButtonEnable by remember {
+                    mutableStateOf(true)
+                }
+                var DeleteButtonEnable by remember {
+                    mutableStateOf(false)
+                }
+                Button(onClick = {++count},
+                    enabled = SaveButtonEnable,
+
+                ) {
+                    if (count >= 15) {
+                        SaveButtonEnable = false
+                    } else {
+                        SaveButtonEnable = true
+                    }
+                    Text(text = "저장", fontSize = 24.sp)
+                }
+                Button(onClick = {--count},
+                    enabled = DeleteButtonEnable
+                ) {
+                    if (count > 0) {
+                        DeleteButtonEnable = true
+                    } else {
+                        DeleteButtonEnable = false
+                    }
+                    Text(text = "삭제", fontSize = 24.sp)
+                }
+                for(i in 1..count) {
+                    Text(text = "$i: 저장했어요", fontSize = 24.sp)
+
+                }
             }
-        }
-    }
-    @Composable
-    fun ButtonReact() {
-        var count by remember {
-            mutableStateOf(0)
-        }
-        Button(onClick = {
-            ++count
-        }) {
-            Text(text = "저장", fontSize = 24.sp)
-        }
-        for(i in 1..count) {
-            Text(text = "$i: 저장했어요", fontSize = 24.sp)
         }
     }
 }
 
+//@Composable
+//fun ButtonReact() {
+//}
