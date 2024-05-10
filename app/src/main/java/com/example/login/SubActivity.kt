b3511d80cea9a3a156dabd7ffb8e95a693407df3
Lastbullet0520 <wjswndgk@gmail.com>
2024-05-10 11:48:35 +0900

    버튼 내 값 저장 연습

--- app/src/main/java/com/example/login/SubActivity.kt
+++ app/src/main/java/com/example/login/SubActivity.kt
@@ -12,7 +12,12 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.vector.rememberVectorPainter
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.sp
 
@@ -31,10 +36,15 @@ class SubActivity : ComponentActivity() {
     }
     @Composable
     fun SubButton(name: String) {
+        var count by remember {
+            mutableStateOf(0)  // 숫자 0으로 지정해놓고 변하는 값을 저장. 이렇게 안하면 계속 0으로 초기화
+        }
         Button(onClick = {
+            ++count
             Toast.makeText(this@SubActivity, name, LENGTH_SHORT).show()
         }) {
             Text(text = name, fontSize = 16.sp)
+            Text(text = count.toString())
         }
     }
 
@@ -46,7 +56,11 @@ fun CarrotList()    {
     Row {
         Image(painter = painterResource(id = R.drawable.ic_launcher_foreground), contentDescription = null)
         Column {
-
+            Text (text = "Title")
+            Text (text = "Time")
+            Text (text = "Location")
+            Text (text = "Price")
+            Text(text = "Comment")
         }
     }
 }
